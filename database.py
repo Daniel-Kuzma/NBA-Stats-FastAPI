568ed87b95b2647ca5f74b998414cae4c415e5de
Daniel Kuźma <danielkuzma905@gmail.com>
2026-09-21 23:56:51 +0200

    Added timezone into engine

--- database.py
+++ database.py
@@ -3,6 +3,6 @@ from settings import settings
 
 SQL_ALCHEMY_DATABASE_URL = settings.database_url
 
-engine = create_async_engine(SQL_ALCHEMY_DATABASE_URL)
+engine = create_async_engine(SQL_ALCHEMY_DATABASE_URL, connect_args = {"server_settings" : {"timezone" : "Europe/Warsaw"}})
 
 LocalSession = async_sessionmaker(autocommit = False, bind = engine, expire_on_commit = False)
