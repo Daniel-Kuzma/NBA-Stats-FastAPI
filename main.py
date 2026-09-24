8c86ad33671beb84f7ea665820e3c51c367d33ac
Daniel Kuźma <danielkuzma905@gmail.com>
2026-09-24 21:50:12 +0200

    Added sheduler for teams logs, players logs, and sctive players

--- main.py
+++ main.py
@@ -4,8 +4,34 @@ from starlette import status
 from typing import Annotated
 from database import LocalSession
 from routes import admin
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from contextlib import asynccontextmanager
+from seeder import upload_new_player_logs, upload_new_teams_logs, check_active_players
 
-app = FastAPI()
+
+async def upload_teams_logs():
+    async with LocalSession() as db:
+        await upload_new_teams_logs(db = db)
+
+async def upload_players_logs():
+    async with LocalSession() as db:
+        await upload_new_player_logs(db = db)
+
+async def update_active_players():
+    async with LocalSession() as db:
+        await check_active_players(db = db)
+
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(upload_teams_logs, "interval", hours = 1, misfire_grace_time=600, coalesce=True)
+    scheduler.add_job(upload_players_logs, "interval", hours = 1, misfire_grace_time=600, coalesce=True)
+    scheduler.add_job(update_active_players, "cron", hour = 2, misfire_grace_time=600, coalesce=True)
+    scheduler.start()
+    yield
+    scheduler.shutdown()
+
+app = FastAPI(lifespan = lifespan)
 
 @app.get("/", status_code = status.HTTP_200_OK)
 async def healthy_check():
